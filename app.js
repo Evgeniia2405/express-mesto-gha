c3e51861a9637d235fee30c8c9a29d9e57b22be3
evgeniiaanikeeva <evgenia2405@yandex.ru>
2023-01-12 15:45:17 +0300

    PR_13_VAR_02

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -11,6 +20,8 @@ const cardsRouter = require('./routes/cards');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(limiter);
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost:27017/mestodb');
